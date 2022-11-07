d5ae931e4a467beee7cd30dc16e3ad72b3f13c91
Corbin <corbinisnotyou@gmail.com>
2022-11-07 10:42:22 -0500

    twig updateS

---
+++ modules/contrib/current_year/src/Plugin/Filter/CurrentYearFilter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\current_year\Plugin\Filter;
+
+use Drupal\filter\Plugin\FilterBase;
+use Drupal\filter\FilterProcessResult;
+
+/**
+ * FilterBase implementation.
+ *
+ * @Filter(
+ *   id = "filter_current_year",
+ *   title = @Translation("Current Year"),
+ *   description = @Translation("This filter enables &year; to be replaced by the current year."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ * )
+ */
+class CurrentYearFilter extends FilterBase {
+
+  /**
+   * Implementation of the process method for this filter.
+   */
+  public function process($text, $langcode) {
+    return new FilterProcessResult($this->currentYearise($text));
+  }
+
+  /**
+   * Replace &year; with the current year.
+   */
+  private function currentYearise($text = '') {
+    $current_year_regex = '|\&(amp;)?year\;|';
+    $text = preg_replace_callback($current_year_regex,
+      function ($matches) {
+        return date('Y');
+      },
+      $text);
+    return $text;
+  }
+
+}
---
+++ modules/contrib/twig_debugger/README.txt
@@ -0,0 +1,32 @@
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Requirements
+ * Installation
+ * Configuration
+ * Maintainers
+
+INTRODUCTION
+------------
+
+Enables Twig Debugging for your website.
+
+REQUIREMENTS
+------------
+
+This module does not have any dependency on any other module.
+
+INSTALLATION
+------------
+
+Install as normal (see http://drupal.org/documentation/install/modules-themes).
+
+CONFIGURATION
+-------------
+
+This module does not have any configuration.
+
+MAINTAINERS
+-----------
+Neslee Canil Pinto: https://www.drupal.org/u/neslee-canil-pinto
